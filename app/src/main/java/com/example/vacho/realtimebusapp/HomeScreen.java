c54b8c3625d624c4328c375ce2bf6747744c5b27
vachopvp <vachopvp@gmail.com>
2016-03-01 00:30:56 +0100

    Navigation drawer without Item Selection

--- app/src/main/java/com/example/vacho/realtimebusapp/HomeScreen.java
+++ app/src/main/java/com/example/vacho/realtimebusapp/HomeScreen.java
@@ -3,29 +3,27 @@ package com.example.vacho.realtimebusapp;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class HomeScreen extends AppCompatActivity {
+public class HomeScreen extends AppCompatActivity implements HomeScreenFragment.FragmentDrawerListener {
 
+    private HomeScreenFragment homeScreenFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        homeScreenFragment = (HomeScreenFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        homeScreenFragment.setUp(R.id.fragment_navigation_drawer,(DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
+        homeScreenFragment.setDrawerListener(this);
     }
 
     @Override
@@ -48,5 +46,10 @@ public class HomeScreen extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDrawerItemSelected(View view, int position) {
+
     }
 }
