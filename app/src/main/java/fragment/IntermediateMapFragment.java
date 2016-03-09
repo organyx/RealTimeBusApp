83d2308930ea591cbf61a8ea0b75fc34446934d4
organyx <aleksandr.beliun@gmail.com>
2016-03-09 19:19:09 +0100

    IntermediateMap with autocomplete suggestions
    Can add new markers based on the suggestions

--- app/src/main/java/fragment/IntermediateMapFragment.java
+++ app/src/main/java/fragment/IntermediateMapFragment.java
@@ -5,27 +5,77 @@ import android.app.Fragment;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.vacho.realtimebusapp.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Map;
+
+import adapter.PlaceArrayAdapter;
 
 /**
  * Created by Aleks on 09-Mar-16.
  */
-public class IntermediateMapFragment extends Fragment implements OnMapReadyCallback {
+public class IntermediateMapFragment extends Fragment implements OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
+
+    private static final String LOG_TAG = "IntrMapFragment";
+    private static final int GOOGLE_API_CLIENT_ID = 0;
+    private AutoCompleteTextView mAutocompleteTextView;
+    private TextView mNameTextView;
+    private TextView mAddressTextView;
+    private GoogleApiClient mGoogleApiClient;
+    private PlaceArrayAdapter mPlaceArrayAdapter;
+    private static final LatLngBounds BOUNDS_MOUNTAIN_VIEW = new LatLngBounds(
+            new LatLng(37.398160, -122.180831), new LatLng(37.430610, -121.972090));
+
+
+    private GoogleMap map;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_intermediate_map,container,false);
+        View v = inflater.inflate(R.layout.fragment_intermediate_map,container,false);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addApi(Places.GEO_DATA_API)
+//                .enableAutoManage(this, GOOGLE_API_CLIENT_ID, this)
+                .addConnectionCallbacks(this)
+                .build();
+        mAutocompleteTextView = (AutoCompleteTextView) v.findViewById(R.id
+                .atv_autocomplete_fav_item_address);
+        mAutocompleteTextView.setThreshold(3);
+        mNameTextView = (EditText) v.findViewById(R.id.et_fav_item_name);
+//        mAddressTextView = (EditText) v.findViewById(R.id.et_);
+
+        mAutocompleteTextView.setOnItemClickListener(mAutocompleteClickListener);
+        mPlaceArrayAdapter = new PlaceArrayAdapter(getActivity(), android.R.layout.simple_list_item_1,
+                BOUNDS_MOUNTAIN_VIEW, null);
+        mAutocompleteTextView.setAdapter(mPlaceArrayAdapter);
+
+        return v;
     }
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
@@ -33,18 +83,105 @@ public class IntermediateMapFragment extends Fragment implements OnMapReadyCallb
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        MapFragment fragment = (MapFragment)getChildFragmentManager().findFragmentById(R.id.f_intermediate_map);
+        MapFragment fragment = (MapFragment) getChildFragmentManager().findFragmentById(R.id.f_intermediate_map);
         fragment.getMapAsync(this);
+//        MapFragment fragment = (MapFragment) getFragmentManager().findFragmentById(R.id.f_intermediate_map);
+//        fragment.getMapAsync(this);
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        LatLng horsens = new LatLng(55.866, 9.833);
+//        LatLng horsens = new LatLng(55.866, 9.833);
+//
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(horsens, 13));
+//
+//        googleMap.addMarker(new MarkerOptions()
+//                .title("Horsens")
+//                .position(horsens));
+        map = googleMap;
+    }
 
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(horsens, 13));
+    private AdapterView.OnItemClickListener mAutocompleteClickListener
+            = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            final PlaceArrayAdapter.PlaceAutocomplete item = mPlaceArrayAdapter.getItem(position);
+            final String placeId = String.valueOf(item.placeId);
+            Log.i(LOG_TAG, "Selected: " + item.description);
+            PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi
+                    .getPlaceById(mGoogleApiClient, placeId);
+            placeResult.setResultCallback(mUpdatePlaceDetailsCallback);
+            Log.i(LOG_TAG, "Fetching details for ID: " + item.placeId);
+        }
+    };
 
-        googleMap.addMarker(new MarkerOptions()
-                .title("Horsens")
-                .position(horsens));
+    private ResultCallback<PlaceBuffer> mUpdatePlaceDetailsCallback
+            = new ResultCallback<PlaceBuffer>() {
+        @Override
+        public void onResult(PlaceBuffer places) {
+            if (!places.getStatus().isSuccess()) {
+                Log.e(LOG_TAG, "Place query did not complete. Error: " +
+                        places.getStatus().toString());
+                return;
+            }
+            // Selecting the first object buffer.
+            final Place place = places.get(0);
+            CharSequence attributions = places.getAttributions();
+
+            mNameTextView.setText(Html.fromHtml(place.getName() + ""));
+//            mAddressTextView.setText(Html.fromHtml(place.getAddress() + ""));
+            addMarker(place);
+        }
+    };
+
+    private void addMarker(Place place)
+    {
+        String name = place.getName() + "";
+        LatLng newLoc = place.getLatLng();
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(newLoc, 13));
+        map.addMarker(new MarkerOptions().title(name).position(newLoc));
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(horsens, 13));
+//
+//        googleMap.addMarker(new MarkerOptions()
+//                .title("Horsens")
+//                .position(horsens));
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mPlaceArrayAdapter.setGoogleApiClient(mGoogleApiClient);
+        Log.i(LOG_TAG, "Google Places API connected.");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mPlaceArrayAdapter.setGoogleApiClient(null);
+        Log.e(LOG_TAG, "Google Places API connection suspended.");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e(LOG_TAG, "Google Places API connection failed with error code: "
+                + connectionResult.getErrorCode());
+
+        Toast.makeText(getActivity(),
+                "Google Places API connection failed with error code:" +
+                        connectionResult.getErrorCode(),
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
     }
 }
