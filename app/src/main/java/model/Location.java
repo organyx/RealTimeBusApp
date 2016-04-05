7f69f4c2546dfdc6068417f0a4ca7b07619138af
organyx <aleksandr.beliun@gmail.com>
2016-04-05 22:04:41 +0200

    Created new DB tables for locations and routes. Minor bug fix

---
+++ app/src/main/java/model/Location.java
@@ -0,0 +1,115 @@
+package model;
+
+/**
+ * Created by Aleks on 05-Apr-16.
+ */
+public class Location {
+
+    private boolean showNotify;
+    private long id;
+    private String name;
+    private String address;
+    private double lat;
+    private double lng;
+    private String zoom;
+    private boolean isFavourited;
+
+    public Location(long id, String name, String address, double lat, double lng, String zoom, boolean isFavourited) {
+        this.isFavourited = isFavourited;
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.lat = lat;
+        this.lng = lng;
+        this.zoom = zoom;
+    }
+
+    public Location(String name, String address, double lat, double lng, String zoom, boolean isFavourited) {
+        this.name = name;
+        this.address = address;
+        this.lat = lat;
+        this.lng = lng;
+        this.zoom = zoom;
+        this.isFavourited = isFavourited;
+    }
+
+    public Location() {
+    }
+
+    public boolean isShowNotify() {
+        return showNotify;
+    }
+
+    public void setShowNotify(boolean showNotify) {
+        this.showNotify = showNotify;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public String getZoom() {
+        return zoom;
+    }
+
+    public void setZoom(String zoom) {
+        this.zoom = zoom;
+    }
+
+    public boolean isFavourited() {
+        return isFavourited;
+    }
+
+    public void setIsFavourited(boolean isFavourited) {
+        this.isFavourited = isFavourited;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", lng=" + lng +
+                ", lat=" + lat +
+                ", zoom='" + zoom + '\'' +
+                ", isFavourited=" + isFavourited +
+                '}';
+    }
+}
--- app/src/main/java/utils/DatabaseHelper.java
+++ app/src/main/java/utils/DatabaseHelper.java
@@ -11,25 +11,72 @@ import java.util.ArrayList;
 import java.util.List;
 
 import model.FavoriteItem;
+import model.Location;
 
 /**
  * Created by Aleks on 05-Mar-16.
  */
 public class DatabaseHelper {
 
+    private static final String TAG = "DatabaseHelper";
+
     private SQLiteOpenHelper _opeSqLiteOpenHelper;
 
-    private static final String DATABASE_NAME = "Favourites.db";
-    private static final String TABLE_NAME = "favourites_table";
-    private static final String COL_ID = "ID";
-    private static final String COL_NAME = "NAME";
-    private static final String COL_ADDRESS = "ADDRESS";
-    private static final String COL_LAT = "LAT";
-    private static final String COL_LNG = "LNG";
-    private static final String COL_ZOOM = "ZOOM";
+    private static final String DATABASE_NAME = "Locations.db";
 
-    private String[] allColumns = { COL_ID,
-            COL_NAME, COL_ADDRESS, COL_LAT, COL_LNG, COL_LNG };
+    private static final String FAVOURITES = "FAVOURITES";
+    private static final String FAVOURITES_COL_ID = "FAVOURITES_COL_ID";
+    private static final String FAVOURITES_COL_NAME = "FAVOURITES_COL_NAME";
+    private static final String FAVOURITES_COL_ADDRESS = "FAVOURITES_COL_ADDRESS";
+    private static final String FAVOURITES_COL_LAT = "FAVOURITES_COL_LAT";
+    private static final String FAVOURITES_COL_LNG = "FAVOURITES_COL_LNG";
+    private static final String FAVOURITES_COL_ZOOM = "FAVOURITES_COL_ZOOM";
+    private static final String CREATE_TABLE_FAVOURITES = "create table " + FAVOURITES + " (" +
+            FAVOURITES_COL_ID + " integer primary key autoincrement, " +
+            FAVOURITES_COL_NAME + " text, " +
+            FAVOURITES_COL_ADDRESS + " text, " +
+            FAVOURITES_COL_LAT + " double, " +
+            FAVOURITES_COL_LNG + " double, " +
+            FAVOURITES_COL_ZOOM + " text )";
+
+    private static final String LOCATIONS = "LOCATIONS";
+    private static final String LOCATIONS_COL_ID = "LOCATIONS_COL_ID";
+    private static final String LOCATIONS_COL_NAME = "LOCATIONS_COL_NAME";
+    private static final String LOCATIONS_COL_ADDRESS = "LOCATIONS_COL_ADDRESS";
+    private static final String LOCATIONS_COL_LAT = "LOCATIONS_COL_LAT";
+    private static final String LOCATIONS_COL_LNG = "LOCATIONS_COL_LNG";
+    private static final String LOCATIONS_COL_ZOOM = "LOCATIONS_COL_ZOOM";
+    private static final String LOCATIONS_COL_FAVORITED = "LOCATIONS_COL_FAVORITED";
+    private static final String CREATE_TABLE_LOCATIONS = "create table " + LOCATIONS + " (" +
+            LOCATIONS_COL_ID + " integer primary key autoincrement, " +
+            LOCATIONS_COL_NAME + " text, " +
+            LOCATIONS_COL_ADDRESS + " text, " +
+            LOCATIONS_COL_LAT + " double, " +
+            LOCATIONS_COL_LNG + " double, " +
+            LOCATIONS_COL_ZOOM + " text, " +
+            LOCATIONS_COL_FAVORITED + " integer )";
+
+    private static final String BUS_LINE = "BUS_LINE";
+    private static final String BUS_LINE_COL_ID = "BUS_LINE_COL_ID";
+    private static final String BUS_LINE_COL_NAME = "BUS_LINE_COL_NAME";
+    private static final String CREATE_TABLE_BUS_LINES = "create table " + BUS_LINE_COL_ID + " (" +
+            LOCATIONS_COL_ID + " integer primary key autoincrement, " +
+            BUS_LINE_COL_NAME + " text )";
+
+    private static final String BUS_LINE_BUS_STOPS = "BUS_LINE_BUS_STOPS";
+    private static final String BUS_LINE_BUS_STOPS_COL_BUS_LINE_ID = "BUS_LINE_BUS_STOPS_COL_BUS_LINE_ID";
+    private static final String BUS_LINE_BUS_STOPS_COL_LOCATION_ID = "BUS_LINE_BUS_STOPS_COL_LOCATION_ID";
+    private static final String CREATE_TABLE_BUS_LINE_BUS_STOPS = "create table " + BUS_LINE_BUS_STOPS + " (" +
+            BUS_LINE_BUS_STOPS_COL_BUS_LINE_ID + " integer, " +
+            BUS_LINE_BUS_STOPS_COL_LOCATION_ID + " integer, " +
+            "CONSTRAINT pk_route_bus_stop PRIMARY KEY ("+BUS_LINE_BUS_STOPS_COL_BUS_LINE_ID+", "+BUS_LINE_BUS_STOPS_COL_LOCATION_ID+")," +
+            "CONSTRAINT fk_route_bus_stop_bus_line_id FOREIGN KEY ("+BUS_LINE_BUS_STOPS_COL_BUS_LINE_ID+") REFERENCES "+ BUS_LINE +"("+BUS_LINE_COL_ID+") ON DELETE CASCADE," +
+            "CONSTRAINT fk_route_bus_stop_bus_stop_id FOREIGN KEY ("+BUS_LINE_BUS_STOPS_COL_LOCATION_ID+") REFERENCES "+ LOCATIONS +"("+LOCATIONS_COL_ID+") ON DELETE CASCADE)";
+
+    private static final int DATABASE_VERSION = 2;
+
+    private String[] allColumns = {FAVOURITES_COL_ID,
+            FAVOURITES_COL_NAME, FAVOURITES_COL_ADDRESS, FAVOURITES_COL_LAT, FAVOURITES_COL_LNG, FAVOURITES_COL_LNG};
 
     public DatabaseHelper(Context context){
         _opeSqLiteOpenHelper = new mySQLiteOpenHelper(context);
@@ -37,62 +84,35 @@ public class DatabaseHelper {
 
     class mySQLiteOpenHelper extends SQLiteOpenHelper {
         mySQLiteOpenHelper(Context context) {
-            super(context, DATABASE_NAME, null, 1);
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
         }
 
         @Override
         public void onCreate(SQLiteDatabase db)
         {
-            db.execSQL("create table " + TABLE_NAME + " (" +
-                    COL_ID + " integer primary key autoincrement, " +
-                    COL_NAME + " text, " +
-                    COL_ADDRESS + " text, " +
-                    COL_LAT + " double, " +
-                    COL_LNG + " double, " +
-                    COL_ZOOM + " text )");
+            Log.d(TAG, "onCreate");
+            db.execSQL(CREATE_TABLE_FAVOURITES);
+            db.execSQL(CREATE_TABLE_LOCATIONS);
+            db.execSQL(CREATE_TABLE_BUS_LINES);
+            db.execSQL(CREATE_TABLE_BUS_LINE_BUS_STOPS);
         }
 
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+            Log.d(TAG, "onUpgrade");
+            db.execSQL("DROP TABLE IF EXISTS " + FAVOURITES);
+            db.execSQL("DROP TABLE IF EXISTS " + LOCATIONS);
+            db.execSQL("DROP TABLE IF EXISTS " + BUS_LINE);
+            db.execSQL("DROP TABLE IF EXISTS " + BUS_LINE_BUS_STOPS);
             onCreate(db);
         }
-    }
-
-    public Cursor getAll()
-    {
-        SQLiteDatabase db = _opeSqLiteOpenHelper.getReadableDatabase();
-        if(db == null)
-        {
-            return null;
-        }
-        return db.rawQuery("SELECT * FROM " + TABLE_NAME + " order by " + COL_ID + "", null);
-    }
-
-    public ContentValues getAll2() {
-        SQLiteDatabase db = _opeSqLiteOpenHelper.getReadableDatabase();
-        if (db == null) {
-            return null;
-        }
-        ContentValues row = new ContentValues();
-        Cursor cur = db.rawQuery("select * from " + TABLE_NAME, null);
-        if (cur.moveToNext()) {
-            row.put("Name", cur.getString(1));
-            row.put("Address", cur.getString(2));
-            row.put("Lat", cur.getDouble(3));
-            row.put("Lng", cur.getDouble(4));
-            row.put("Zoom", cur.getString(5));
-        }
-        cur.close();
-        db.close();
-        return row;
     }
 
     public List<FavoriteItem> getAllFavourites()
     {
         SQLiteDatabase db = _opeSqLiteOpenHelper.getReadableDatabase();
         List<FavoriteItem> favoriteItemList = new ArrayList<FavoriteItem>();
-        Cursor cursor = db.query(TABLE_NAME, allColumns, null, null, null, null, null );
+        Cursor cursor = db.query(FAVOURITES, allColumns, null, null, null, null, null );
         cursor.moveToFirst();
         while (!cursor.isAfterLast())
         {
@@ -102,7 +122,7 @@ public class DatabaseHelper {
         }
 
         cursor.close();
-        Log.d("DB_Helper", "DB Items retrieved = " + favoriteItemList.size());
+        Log.d(TAG, "DB Items retrieved = " + favoriteItemList.size());
         return favoriteItemList;
     }
 
@@ -114,20 +134,20 @@ public class DatabaseHelper {
             return 0;
         }
         ContentValues row = new ContentValues();
-        if(!nameExists(name)) {
-            row.put(COL_NAME, name);
-            row.put(COL_ADDRESS, address);
-            row.put(COL_LAT, lat);
-            row.put(COL_LNG, lng);
-            row.put(COL_ZOOM, zoom);
-            long id = db.insert(TABLE_NAME, null, row);
+        if(!nameExists(name, FAVOURITES)) {
+            row.put(FAVOURITES_COL_NAME, name);
+            row.put(FAVOURITES_COL_ADDRESS, address);
+            row.put(FAVOURITES_COL_LAT, lat);
+            row.put(FAVOURITES_COL_LNG, lng);
+            row.put(FAVOURITES_COL_ZOOM, zoom);
+            long id = db.insert(FAVOURITES, null, row);
             db.close();
-            Log.d("DB_Helper", "DB Inserted ID = " + id + " NAME = " + name + " ADDRESS = " + address + " LAT = " + lat + " LNG = " + lng + " ZOOM = " + zoom);
+            Log.d(TAG, "DB Inserted ID = " + id + " NAME = " + name + " ADDRESS = " + address + " LAT = " + lat + " LNG = " + lng + " ZOOM = " + zoom);
             return id;
         }
         else
         {
-            Log.d("DB_Helper", "DB Inserted = " + 0);
+            Log.d(TAG, "DB Inserted = " + 0);
             return 0;
         }
 
@@ -138,12 +158,12 @@ public class DatabaseHelper {
         SQLiteDatabase db = _opeSqLiteOpenHelper.getWritableDatabase();
         if(db==null)
             return;
-        int deleted = db.delete(TABLE_NAME, COL_NAME + " = ? ", new String[]{String.valueOf(name)});
+        int deleted = db.delete(FAVOURITES, FAVOURITES_COL_NAME + " = ? ", new String[]{String.valueOf(name)});
 
         if(deleted > 0)
-            Log.d("DB_Helper", "DB Deleted ID = " + deleted);
+            Log.d(TAG, "DB Deleted ID = " + deleted);
         else
-            Log.d("DB_Helper", "DB Deleted = " + deleted);
+            Log.d(TAG, "DB Deleted = " + deleted);
 
         db.close();
     }
@@ -156,48 +176,48 @@ public class DatabaseHelper {
             return;
         }
         ContentValues row = new ContentValues();
-        row.put(COL_NAME, name);
-        row.put(COL_ADDRESS, address);
+        row.put(FAVOURITES_COL_NAME, name);
+        row.put(FAVOURITES_COL_ADDRESS, address);
         if(lat != 0 && lng != 0)
         {
-            row.put(COL_LAT, lat);
-            row.put(COL_LNG, lng);
-            row.put(COL_ZOOM, zoom);
+            row.put(FAVOURITES_COL_LAT, lat);
+            row.put(FAVOURITES_COL_LNG, lng);
+            row.put(FAVOURITES_COL_ZOOM, zoom);
         }
-        int updated = db.update(TABLE_NAME, row, COL_NAME + " = ?", new String[]{String.valueOf(name)});
+        int updated = db.update(FAVOURITES, row, FAVOURITES_COL_NAME + " = ?", new String[]{String.valueOf(name)});
 
         if(updated > 0)
-            Log.d("DB_Helper", "DB Updated ID = " + updated);
+            Log.d(TAG, "DB Updated ID = " + updated);
         else
-            Log.d("DB_Helper", "DB Updated = " + updated);
+            Log.d(TAG, "DB Updated = " + updated);
         db.close();
     }
 
-    public boolean nameExists(String name)
+    public boolean nameExists(String name, String tableName)
     {
         SQLiteDatabase db = _opeSqLiteOpenHelper.getWritableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + COL_NAME + " = ?;", new String[] {name});
+        Cursor cursor = db.rawQuery("SELECT * FROM " + tableName + " WHERE " + tableName + "_COL_NAME = ?;", new String[] {name});
         boolean exists = (cursor.getCount() > 0);
         cursor.close();
         return  exists;
     }
 
-    public boolean isEmpty()
+    public boolean isEmpty(String table_name)
     {
         SQLiteDatabase db = _opeSqLiteOpenHelper.getWritableDatabase();
-        Cursor cursor = db.rawQuery("SELECT COUNT(*) FROM " + TABLE_NAME, null);
+        Cursor cursor = db.rawQuery("SELECT COUNT(*) FROM " + table_name, null);
         cursor.moveToFirst();
         int count = cursor.getInt(0);
         boolean empty;
         if(count > 0)
         {
             empty = false;
-            Log.d("DB_Helper", "DB Empty = " + empty);
+            Log.d(TAG, "DB Table " + table_name + " Empty? = " + empty);
         }
         else
         {
             empty = true;
-            Log.d("DB_Helper", "DB Empty = " + empty);
+            Log.d(TAG, "DB Table " + table_name + " Empty? = " + empty);
         }
         cursor.close();
         return empty;
@@ -212,5 +232,17 @@ public class DatabaseHelper {
         favoriteItem.setLng(cursor.getDouble(4));
         favoriteItem.setZoom(cursor.getString(5));
         return favoriteItem;
+    }
+
+    private Location cursorToLocation(Cursor cursor) {
+        Location location = new Location();
+        location.setId(cursor.getLong(0));
+        location.setName(cursor.getString(1));
+        location.setAddress(cursor.getString(2));
+        location.setLat(cursor.getDouble(3));
+        location.setLng(cursor.getDouble(4));
+        location.setZoom(cursor.getString(5));
+        location.setIsFavourited(cursor.getInt(6) != 0);
+        return location;
     }
 }
